e62738aa1763b0116a7851926fb15a61618fc61e
Antonina <novikovantoninalex@gmail.com>
2025-04-07 15:23:40 +0300

    Add changes

--- humans.py
+++ humans.py
@@ -7,114 +7,118 @@ import file_operations
 
 
 SKILLS = [
-    'Стремительный прыжок',
-    'Электрический выстрел',
-    'Ледяной удар',
-    'Стремительный удар',
-    'Кислотный взгляд',
-    'Тайный побег',
-    'Ледяной выстрел',
-    'Огненный заряд'
+    "Стремительный прыжок",
+    "Электрический выстрел",
+    "Ледяной удар",
+    "Стремительный удар",
+    "Кислотный взгляд",
+    "Тайный побег",
+    "Ледяной выстрел",
+    "Огненный заряд",
 ]
 
 MAPPING = {
-    'а': 'а͠',
-    'б': 'б̋',
-    'в': 'в͒͠',
-    'г': 'г͒͠',
-    'д': 'д̋',
-    'е': 'е͠',
-    'ё': 'ё͒͠',
-    'ж': 'ж͒',
-    'з': 'з̋̋͠',
-    'и': 'и',
-    'й': 'й͒͠',
-    'к': 'к̋̋',
-    'л': 'л̋͠',
-    'м': 'м͒͠',
-    'н': 'н͒',
-    'о': 'о̋',
-    'п': 'п̋͠',
-    'р': 'р̋͠',
-    'с': 'с͒',
-    'т': 'т͒',
-    'у': 'у͒͠',
-    'ф': 'ф̋̋͠',
-    'х': 'х͒͠',
-    'ц': 'ц̋',
-    'ч': 'ч̋͠',
-    'ш': 'ш͒͠',
-    'щ': 'щ̋',
-    'ъ': 'ъ̋͠',
-    'ы': 'ы̋͠',
-    'ь': 'ь̋',
-    'э': 'э͒͠͠',
-    'ю': 'ю̋͠',
-    'я': 'я̋',
-    'А': 'А͠',
-    'Б': 'Б̋',
-    'В': 'В͒͠',
-    'Г': 'Г͒͠',
-    'Д': 'Д̋',
-    'Е': 'Е',
-    'Ё': 'Ё͒͠',
-    'Ж': 'Ж͒',
-    'З': 'З̋̋͠',
-    'И': 'И',
-    'Й': 'Й͒͠',
-    'К': 'К̋̋',
-    'Л': 'Л̋͠',
-    'М': 'М͒͠',
-    'Н': 'Н͒',
-    'О': 'О̋',
-    'П': 'П̋͠',
-    'Р': 'Р̋͠',
-    'С': 'С͒',
-    'Т': 'Т͒',
-    'У': 'У͒͠',
-    'Ф': 'Ф̋̋͠',
-    'Х': 'Х͒͠',
-    'Ц': 'Ц̋',
-    'Ч': 'Ч̋͠',
-    'Ш': 'Ш͒͠',
-    'Щ': 'Щ̋',
-    'Ъ': 'Ъ̋͠',
-    'Ы': 'Ы̋͠',
-    'Ь': 'Ь̋',
-    'Э': 'Э͒͠͠',
-    'Ю': 'Ю̋͠',
-    'Я': 'Я̋',
-    ' ': ' '
- }
+    "а": "а͠",
+    "б": "б̋",
+    "в": "в͒͠",
+    "г": "г͒͠",
+    "д": "д̋",
+    "е": "е͠",
+    "ё": "ё͒͠",
+    "ж": "ж͒",
+    "з": "з̋̋͠",
+    "и": "и",
+    "й": "й͒͠",
+    "к": "к̋̋",
+    "л": "л̋͠",
+    "м": "м͒͠",
+    "н": "н͒",
+    "о": "о̋",
+    "п": "п̋͠",
+    "р": "р̋͠",
+    "с": "с͒",
+    "т": "т͒",
+    "у": "у͒͠",
+    "ф": "ф̋̋͠",
+    "х": "х͒͠",
+    "ц": "ц̋",
+    "ч": "ч̋͠",
+    "ш": "ш͒͠",
+    "щ": "щ̋",
+    "ъ": "ъ̋͠",
+    "ы": "ы̋͠",
+    "ь": "ь̋",
+    "э": "э͒͠͠",
+    "ю": "ю̋͠",
+    "я": "я̋",
+    "А": "А͠",
+    "Б": "Б̋",
+    "В": "В͒͠",
+    "Г": "Г͒͠",
+    "Д": "Д̋",
+    "Е": "Е",
+    "Ё": "Ё͒͠",
+    "Ж": "Ж͒",
+    "З": "З̋̋͠",
+    "И": "И",
+    "Й": "Й͒͠",
+    "К": "К̋̋",
+    "Л": "Л̋͠",
+    "М": "М͒͠",
+    "Н": "Н͒",
+    "О": "О̋",
+    "П": "П̋͠",
+    "Р": "Р̋͠",
+    "С": "С͒",
+    "Т": "Т͒",
+    "У": "У͒͠",
+    "Ф": "Ф̋̋͠",
+    "Х": "Х͒͠",
+    "Ц": "Ц̋",
+    "Ч": "Ч̋͠",
+    "Ш": "Ш͒͠",
+    "Щ": "Щ̋",
+    "Ъ": "Ъ̋͠",
+    "Ы": "Ы̋͠",
+    "Ь": "Ь̋",
+    "Э": "Э͒͠͠",
+    "Ю": "Ю̋͠",
+    "Я": "Я̋",
+    " ": " ",
+}
 
-fake = Faker('ru_RU')
+fake = Faker("ru_RU")
 
 
-def apply_mapping(skill, mapping):
-    return ''.join(mapping.get(char, char) for char in skill)
+def apply_MAPPING(skill, MAPPING):
+    return "".join(MAPPING.get(char, char) for char in skill)
 
 
 def main():
-    modified_SKILLS = [apply_mapping(skill, MAPPING) for skill in SKILLS]
-    os.makedirs('output', exist_ok=True)
-    if __name__ == '__main__':
+    modified_SKILLS = [apply_MAPPING(skill, MAPPING) for skill in SKILLS]
+    os.makedirs("output", exist_ok=True)
+
+    if __name__ == "__main__":
         for i in range(10):
             selected_SKILLS = random.sample(modified_SKILLS, 8)
             context = {
-                'first_name': fake.first_name(),
-                'last_name': fake.last_name(),
-                'job': fake.job(),
-                'town': fake.city(),
-                'strength': random.randint(3, 18),
-                'agility': random.randint(3, 18),
-                'endurance': random.randint(3, 18),
-                'intelligence': random.randint(3, 18),
-                'luck': random.randint(3, 18),
-                'skill_1': selected_SKILLS[0],
-                'skill_2': selected_SKILLS[1],
-                'skill_3': selected_SKILLS[2],
+                "first_name": fake.first_name(),
+                "last_name": fake.last_name(),
+                "job": fake.job(),
+                "town": fake.city(),
+                "strength": random.randint(3, 18),
+                "agility": random.randint(3, 18),
+                "endurance": random.randint(3, 18),
+                "intelligence": random.randint(3, 18),
+                "luck": random.randint(3, 18),
+                "skill_1": selected_SKILLS[0],
+                "skill_2": selected_SKILLS[1],
+                "skill_3": selected_SKILLS[2],
             }
 
-    output_filename = f'charsheet-{i}.svg'
-    output_path = os.path.join('output', output_filename)
-    file_operations.render_template('src/charsheet.svg', output_path, context)
+            output_filename = f"charsheet-{i}.svg"
+            output_path = os.path.join("output", output_filename)
+            file_operations.render_template("src/charsheet.svg", output_path, context)
+
+
+main()
